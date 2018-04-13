32cfb23841fcf5d246dba6a4c08284b3d8671b2c
Mentos1105 <38364178+Mentos1105@users.noreply.github.com>
2018-04-13 23:11:01 +0200

    Update Zadanko.py

--- Zadanko.py
+++ Zadanko.py
@@ -10,7 +10,6 @@ def main():
     t = 0  # czas
     deltat = 1/2 / P  # krok czasu
     states = tuple(product(range(-R, R + 1), range(-R, R + 1), range(-P, P + 1), range(-P, P + 1)))
-    #print(states.index((-11, -11, 5, 5)))
     step = (2 * P + 1) ** 2
     print(step)
     border = (2 * R + 1) * step
