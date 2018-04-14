0424c402f9332088961e9a29a7b570f64df8b693
er713 <33582219+er713@users.noreply.github.com>
2018-04-14 15:59:04 +0200

    Add files via upload

--- Zadanko.py
+++ Zadanko.py
@@ -1,5 +1,6 @@
 from itertools import product, cycle
 from random import randrange
+from math import floor
 
 
 def main():
@@ -15,14 +16,56 @@ def main():
     border = (2 * R + 1) * step
     print(border)
     atoms = []
+    ns = [0*i for i in range(len(states))]
     atomsleft = N
     for i in cycle(range(0, border, step)):
         atoms.append(randrange(i, i + step))
+        ns[atoms[len(atoms)-1]] = ns[atoms[len(atoms)-1]]+1
         atomsleft -= 1
         if atomsleft == 0:
             break
     for i in range(N):
         print(states[atoms[i]])
+        # print(ns[atoms[i]])
+
+    def daje_ns_od_tj(j, atoms1, ns1):
+        tj = j*deltat
+        # print(R, P, tj)
+        for i in range(len(atoms1)):
+            xatoms = floor(states[atoms1[i]][0] + tj*states[atoms1[i]][2])
+            yatoms = floor(states[atoms1[i]][1] + tj*states[atoms1[i]][3])
+            ns1[atoms1[i]] = ns1[atoms1[i]] - 1
+            while xatoms >= R or xatoms < -R:
+                if xatoms >= R:
+                    xatoms = 2*R-xatoms # odbicie od ściany
+                    atoms1[i] -= (2 * P + 1) * (2 * abs(states[atoms1[i]][2])) # mnożenie wektora razy -1
+                elif xatoms < -R:
+                    xatoms = -xatoms-2*R # odbicie od ściany
+                    atoms1[i] += (2 * P + 1) * (2 * abs(states[atoms1[i]][2])) # mnożenie wektora razy -1
+            while yatoms >= R or yatoms < -R:
+                if yatoms >= R:
+                    yatoms = 2*R-yatoms # odbicie od ściany
+                    atoms1[i] -= 2*abs(states[atoms1[i]][3]) # mnożenie wektora razy -1
+                elif yatoms < -R:
+                    yatoms = -yatoms-2*R # odbicie od ściany
+                    atoms1[i] += 2*abs(states[atoms1[i]][3]) # mnożenie wektora razy -1
+
+            atoms1[i] += (xatoms - states[atoms1[i]][0])*border + (yatoms - states[atoms1[i]][1])*step # zamiana miejsca atomu w tuple
+            ns1[atoms1[i]] = ns1[atoms1[i]] + 1
+            return ns1
+
+    maxj = int(input())
+    for i in range(maxj):
+        patoms = atoms
+        pns = ns
+        daje_ns_od_tj(i, atoms, ns)
+        # !!! tutaj liczenie tych prawdopodobieńst !!! #
+        atoms = patoms
+        ns = pns
+
+
+
+
 
 
 
